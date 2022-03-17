9f8dc6f0dcba7e564e46ad8b7ea6196b3f0ced6f
AlexMinez <alex_minihan@outlook.com>
2022-03-17 20:33:59 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const removeMain = document.querySelector("#main")
+removeMain.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+
+document.querySelector('h1#victory')
+newHeader.innerHTML = 'Alex is the champion'
